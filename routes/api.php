ab797c20903584900885f145dee70f4e1d81f57c
Liberu Automation <opensource@liberu.co.uk>
2026-08-24 16:24:46 +0000

    Publish Automation package

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Liberu\Modules\Automation\DataProcessing\Api\Http\Controllers\DataProcessingResourceController;
 
-Route::middleware(['api', 'auth:sanctum'])->prefix('api/v1/automation/data-processing')->group(function (): void {
+Route::middleware(['api', 'auth:sanctum', 'throttle:60,1'])->prefix('api/v1/automation/data-processing')->group(function (): void {
     Route::get('/', [DataProcessingResourceController::class, 'index']);
     Route::post('/', [DataProcessingResourceController::class, 'store']);
     Route::get('/{id}', [DataProcessingResourceController::class, 'show']);
